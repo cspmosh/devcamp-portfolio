0e112788f7dccc6523d0b9cc5df1279878bb6a47
Josh Turner <cs.pmosh@gmail.com>
2020-04-09 07:55:22 -0500

    added before_action set_porttfolio_item for edit, update, show, and delete

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,4 +1,6 @@
 class PortfoliosController < ApplicationController
+  before_action :set_portfolio_item, :only ['edit','update','show','destroy']
+
   layout "portfolio"
 
 	def index
@@ -27,11 +29,9 @@ class PortfoliosController < ApplicationController
     end
 
     def edit
-    	@portfolio_item = Portfolio.find(params[:id])
     end
 
     def update
-      @portfolio_item = Portfolio.find(params[:id])
       respond_to do |format|
 	      if @portfolio_item.update(portfolio_params)
 	        format.html { redirect_to portfolios_path, notice: 'Portfolio item was successfully updated.' }
@@ -42,12 +42,9 @@ class PortfoliosController < ApplicationController
     end
 
     def show
-      @portfolio_item = Portfolio.find(params[:id])      
     end
 
     def destroy
-      # perform the lookup
-      @portfolio_item = Portfolio.find(params[:id])
       # destroy the record
       @portfolio_item.destroy
       	respond_to do |format|
@@ -63,5 +60,9 @@ class PortfoliosController < ApplicationController
                                         :body, 
                                         technologies_attributes: [:name]
                                         )
+    end
+
+    def set_portfolio_items
+      @portfolio_item = Portfolio.find(params[:id])
     end
 end
