bc5944228c626dce2185f524d669991a53b46836
Josh Turner <cs.pmosh@gmail.com>
2020-05-13 16:14:14 -0500

    ehhhhh

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -44,6 +44,7 @@ $(function(){
 //= require jquery-ui
 //= require custom/htmlsortable
 //= require cocoon
+//= require gritter
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
