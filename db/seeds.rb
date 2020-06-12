9da0759247d7e4c1b74dd2bdcbff5624b2c4649b
Josh Turner <cs.pmosh@gmail.com>
2020-06-12 08:04:00 -0500

    updated seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,22 @@
+User.create!(
+	email: "test@test.com",
+	password: "asdfasdf",
+	password_confirmation: "asdfasdf",
+	name: "Admin User",
+	roles: "site_admin"
+)
+
+puts "1 admin user created"
+
+User.create!(
+	email: "test2@test.com",
+	password: "asdfasdf",
+	password_confirmation: "asdfasdf",
+	name: "Regular User"
+)
+
+puts "1 guest user created"
+
 3.times do |topic|
 	Topic.create!(
 		title: "Topic #{topic}"
